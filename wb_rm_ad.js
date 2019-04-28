0cb962e1287b7bde0b4e58cd34e19c7a034b85bc
yel <xlitao@icloud.com>
2019-04-28 16:26:48 +0800

    add

--- wb_rm_ad.js
+++ wb_rm_ad.js
@@ -1,8 +1,9 @@
 let path2 = '/2/statuses/';//Feed流广告
 let path3 = '/2/statuses/extend';//微博创作者广告共享计划、相关推荐
+let path4 = '/2/comments/build_comments';//评论中相关内容
 var result = body;
 if (url.indexOf(path2) != -1) {
-    var json_body = JSON.parse(body)
+    var json_body = JSON.parse(body);
     if (url.indexOf(path3) != -1) { 
         delete json_body.trend
     }else {
@@ -20,6 +21,18 @@ if (url.indexOf(path2) != -1) {
                     }
                 }
             }
+        }
+    }
+    result = JSON.stringify(json_body);
+}
+if (url.indexOf(path4) != -1) { 
+    var json_body = JSON.parse(body);
+    var datas = json_body.datas;
+    for (let j = 0; j < datas.length; j++) {
+        const element = datas[j];
+        let type = element.type;
+        if (type == 5) {
+            datas.splice(j,1);
         }
     }
     result = JSON.stringify(json_body);
