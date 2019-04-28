60f16b2fca1e96766a9410732c8829b34b8cdbcc
yel <yichahucha@users.noreply.github.com>
2019-04-28 20:42:05 +0800

    add

--- wb_rm_ad.js
+++ wb_rm_ad.js
@@ -36,7 +36,6 @@ if (url.indexOf(path4) != -1) {
             new_datas.push(element);
         }
     }
-    console.log('相关内容');
     json_body.datas = new_datas;
     result = JSON.stringify(json_body);
 }
