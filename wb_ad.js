e8116c8bd29bd50fa33721275356a9a962c1038d
yel <xlitao@icloud.com>
2019-04-28 13:49:36 +0800

    add

---
+++ wb_ad.js
@@ -0,0 +1,22 @@
+var path1 = '/interface/sdk/sdkad.php';
+var result = JSON.parse(body)
+if (url.indexOf(path1) != -1) {
+    result.needlocation = false;
+    result.show_push_splash_ad = false;
+    result.ads = [];
+} else {
+    var ad = result.ad;
+    var statuses = result.statuses;
+    for (let i = 0; i < ad.length; i++) {
+        const element = ad[i];
+        let ad_id = element.id;
+        for (let j = 0; j < statuses.length; j++) {
+            const element = statuses[j];
+            let statuses_id = element.id;
+            if (statuses_id == parseInt(ad_id)) {
+                statuses.splice(j,1);
+            }
+        }
+    }
+}
+JSON.stringify(result)
