df0516fa28bf2568738571a4f196a6baa02337a9
yel <xlitao@icloud.com>
2019-04-28 14:07:13 +0800

    add

--- wb_ad.js
+++ wb_ad.js
@@ -7,15 +7,17 @@ if (url.indexOf(path1) != -1) {
     console.log('去启动页广告')
 } else {
     var ad = result.ad;
-    var statuses = result.statuses;
-    for (let i = 0; i < ad.length; i++) {
-        const element = ad[i];
-        let ad_id = element.id;
-        for (let j = 0; j < statuses.length; j++) {
-            const element = statuses[j];
-            let statuses_id = element.id;
-            if (statuses_id == parseInt(ad_id)) {
-                statuses.splice(j,1);
+    if (typeof(ad) != "undefined") {
+        var statuses = result.statuses;
+        for (let i = 0; i < ad.length; i++) {
+            const element = ad[i];
+            let ad_id = element.id;
+            for (let j = 0; j < statuses.length; j++) {
+                const element = statuses[j];
+                let statuses_id = element.id;
+                if (statuses_id == parseInt(ad_id)) {
+                    statuses.splice(j,1);
+                }
             }
         }
     }
