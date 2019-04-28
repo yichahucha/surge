d610a4b5016e0f2efc0229c90a007e281db7f223
yel <xlitao@icloud.com>
2019-04-28 14:45:04 +0800

    add

--- wb_ad.js
+++ wb_ad.js
@@ -1,5 +1,7 @@
-let path1 = '/interface/sdk/sdkad.php';
-let path2 = '/2/statuses/';
+let path1 = '/interface/sdk/sdkad.php';//启动页广告
+let path2 = '/2/statuses/';//Feed流广告
+let path3 = '/2/statuses/extend';//微博创作者广告共享计划
+
 var result = body;
 if (url.indexOf(path1) != -1) {
     var json_body = JSON.parse(body)
@@ -7,27 +9,32 @@ if (url.indexOf(path1) != -1) {
     json_body.show_push_splash_ad = false;
     json_body.ads = [];
     result = JSON.stringify(json_body);
-    console.log('去启动页广告');
+    console.log('启动页广告');
 } 
 
 if (url.indexOf(path2) != -1) {
     var json_body = JSON.parse(body)
-    var ad = json_body.ad;
-    if (typeof(ad) != "undefined") {
-        var statuses = json_body.statuses;
-        for (let i = 0; i < ad.length; i++) {
-            const element = ad[i];
-            let ad_id = element.id;
-            for (let j = 0; j < statuses.length; j++) {
-                const element = statuses[j];
-                let statuses_id = element.id;
-                if (statuses_id == parseInt(ad_id)) {
-                    statuses.splice(j,1);
+    if (url.indexOf(path3) != -1) { 
+        json_body.trend = {};
+        console.log('共享计划广告');
+    }else {
+        var ad = json_body.ad;
+        if (typeof(ad) != "undefined") {
+            var statuses = json_body.statuses;
+            for (let i = 0; i < ad.length; i++) {
+                const element = ad[i];
+                let ad_id = element.id;
+                for (let j = 0; j < statuses.length; j++) {
+                    const element = statuses[j];
+                    let statuses_id = element.id;
+                    if (statuses_id == parseInt(ad_id)) {
+                        statuses.splice(j,1);
+                    }
                 }
             }
         }
+        console.log('Feed流广告');
     }
     result = JSON.stringify(json_body);
-    console.log('去应用呢广告');
 }
 result;
