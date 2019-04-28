4afaf41ddd05b11c50fa53aa4e9c5d6a7cd87357
yel <xlitao@icloud.com>
2019-04-28 15:38:39 +0800

    add

--- wb_ad.js
+++ wb_ad.js
@@ -1,21 +1,20 @@
 let path1 = '/interface/sdk/sdkad.php';//启动页广告
 let path2 = '/2/statuses/';//Feed流广告
-let path3 = '/2/statuses/extend';//微博创作者广告共享计划
-
+let path3 = '/2/statuses/extend';//微博创作者广告共享计划、相关推荐
 var result = body;
 if (url.indexOf(path1) != -1) {
-    var json_body = JSON.parse(body)
-    json_body.needlocation = false;
+    result = body.substring(0, body.length-2); 
+    var json_body = JSON.parse(result)
     json_body.show_push_splash_ad = false;
     json_body.ads = [];
-    result = JSON.stringify(json_body);
+    result = JSON.stringify(json_body)+'OK';
     console.log('启动页广告');
+    console.log(result);
 } 
-
 if (url.indexOf(path2) != -1) {
     var json_body = JSON.parse(body)
     if (url.indexOf(path3) != -1) { 
-        delete json_body.trend;
+        delete json_body.trend
         console.log('共享计划广告');
     }else {
         var ad = json_body.ad;
