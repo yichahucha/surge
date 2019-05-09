77332343e436c3be1bff167cdc4bb3a6f6254eef
yel <xlitao@icloud.com>
2019-05-09 15:46:56 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -75,13 +75,13 @@ if (url.indexOf(path10) != -1) {
     filter_timeline();
 }
 
-if (url.indexOf(path3) != -1) {
-    let obj = JSON.parse(body);
-    if (obj.trend) {
-        obj.trend = {};
-    }
-    result = JSON.stringify(obj);
-}
+// if (url.indexOf(path3) != -1) {
+//     let obj = JSON.parse(body);
+//     if (obj.trend) {
+//         obj.trend = {};
+//     }
+//     result = JSON.stringify(obj);
+// }
 
 if (url.indexOf(path4) != -1) {
     let obj = JSON.parse(body);
