bec7d114b4b41af0457ea2619e27187bd3ee0cb9
yel <xlitao@icloud.com>
2019-05-09 15:19:04 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -1,5 +1,4 @@
 /*
- * @repo: https://github.com/yichahucha/surge
  * @script: https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
  * @regular: ^https?:\/\/api\.weibo\.cn\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline)
  */
