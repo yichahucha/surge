96cfa016688a5cb0d61e2df308520c968cb09885
yel <xlitao@icloud.com>
2019-05-09 15:24:05 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -77,7 +77,8 @@ if (url.indexOf(path10) != -1) {
 
 if (url.indexOf(path3) != -1) {
     let obj = JSON.parse(body);
-    delete obj.trend;
+//     delete obj.trend;
+    obj.trend = [];
     result = JSON.stringify(obj);
 }
 
