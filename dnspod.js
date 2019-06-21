a576a68d0e794655587e03b2d35e2fbdcda7b50b
yel <xlitao@icloud.com>
2019-06-21 16:39:17 +0800

    de

--- dnspod.js
+++
@@ -1,7 +0,0 @@
-$httpClient.get('http://119.29.29.29/d?dn=' + $domain, function(error, response, data){
-  if (error) {
-    $done({}); // Fallback to standard DND query
-  } else {
-    $done({addresses: data.split(';'), ttl: 600});
-  }
-});
