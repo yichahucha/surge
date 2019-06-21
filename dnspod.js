0f7a152005e4e7042696ccfce84402f10fd5b840
yel <xlitao@icloud.com>
2019-06-21 16:16:48 +0800

    fix

--- dnspod.js
+++ dnspod.js
@@ -1,4 +1,4 @@
-$httpClient.get('http://119.29.29.29/d?dn=' + $domain + '&ip=1.1.1.1', function(error, response, data){
+$httpClient.get('http://119.29.29.29/d?dn=' + $domain, function(error, response, data){
   if (error) {
     $done({}); // Fallback to standard DND query
   } else {
