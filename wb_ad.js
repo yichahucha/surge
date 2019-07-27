475dfd941a4c2ab7c175e9b19f4f8a23fd8fbabb
0x1 <xlitao@icloud.com>
2019-07-27 11:56:22 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -144,7 +144,7 @@ if (url.indexOf(path12) != -1 || url.indexOf(path13) != -1) {
         let i = cards.length;
         while (i--) {
             let element = cards[i];
-            if (is_timeline_ad(element.promotion)) cards.splice(i, 1);
+            if (is_timeline_ad(element.mblog.promotion)) cards.splice(i, 1);
         }
     }
     body = JSON.stringify(obj);
