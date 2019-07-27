7a1174e02fda39ed4bddc05a78fc2c9fe8699fc8
0x1 <xlitao@icloud.com>
2019-07-27 15:57:42 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -151,7 +151,8 @@ function filter_timeline_cards(cards) {
                     } else {
                         let card_type = element.card_type;
                         if (card_type && (card_type == 22 || card_type == 19)) {
-//                             card_group.splice(i, 1);
+                            cards.splice(i, 1);
+                            break;
                         }
                     }
                 }
