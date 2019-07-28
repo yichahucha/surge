314cb8ba1cec48ecf488faa752682d42ff0dc3a4
0x1 <xlitao@icloud.com>
2019-07-28 22:00:08 +0800

    fix

--- wb_ad.js
+++ wb_ad.js
@@ -151,7 +151,7 @@ function filter_timeline_cards(cards) {
                 while (i--) {
                     let element = card_group[i];
                     let card_type = element.card_type;
-                    if (card_type && (tem_card_type == 22 || card_type == 19)) {
+                    if (card_type && (tem_card_type == 22 && card_type == 19)) {
                         cards.splice(j, 1);
                         break;
                     } else if (card_type && card_type == 118) {
