3583cf52eef6c6dad4a3ae1eb793ac5c1c71ac35
0x1 <xlitao@icloud.com>
2019-07-29 17:28:36 +0800

    fix

--- cron_daily.js
+++ cron_daily.js
@@ -1,4 +1,6 @@
-//每日壹句（有道）+ 每日打卡提醒
+/**
+    每日壹句（有道词典）+ 每日打卡提醒（例如：corn "0 9,18 * * 1-5" 周一到周五，早九晚六）
+**/
 $httpClient.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
     let obj = JSON.parse(data);
     let date = new Date();
