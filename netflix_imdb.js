44e4164a74de3b369cdeda8eda9b3eed77eda37b
0x1 <xlitao@icloud.com>
2019-09-22 14:57:04 +0800

    fix

--- netflix_imdb.js
+++ netflix_imdb.js
@@ -28,7 +28,7 @@ if ($request.headers) {
         if (rating) {
             let imdbRating = rating.imdbRating;
             let imdbVotes = rating.imdbVotes;
-            let rating_message = "IMDb: ⭐️ " + imdbRating + "/10  " + imdbVotes;
+            let rating_message = "IMDb: ⭐️ " + imdbRating + "/10.0  " + imdbVotes;
             let summary = obj.value.videos[video_id].summary;
             if (summary && summary.supplementalMessage) {
                 summary.supplementalMessage =
