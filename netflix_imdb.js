00b0386e9ea5a206907981004a06e1f13ec4a03c
0x1 <xlitao@icloud.com>
2019-09-22 14:59:31 +0800

    fix

--- netflix_imdb.js
+++ netflix_imdb.js
@@ -1,3 +1,11 @@
+/*
+[Script]
+http-response ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D script-path=https://raw.githubusercontent.com/yichahucha/surge/master/netflix_imdb.js,requires-body=1
+http-request ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D script-path=https://raw.githubusercontent.com/yichahucha/surge/master/netflix_imdb.js
+
+[MITM]
+hostname = ios.prod.ftl.netflix.com
+*/
 const imdb_api_key = "f75e0253";
 const netflix_title_cache_key = "netflix_title_map";
 
