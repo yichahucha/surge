d5c0601fd4006dfef72598578d601d9316ef583c
nil <xlitao@icloud.com>
2019-10-22 13:59:04 +0800

    Update NetflixRatings.js

--- NetflixRatings.js
+++ NetflixRatings.js
@@ -1,7 +1,7 @@
 /*
 [Script]
 http-request ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D script-path=https://raw.githubusercontent.com/yichahucha/surge/master/NetflixRatings.js
-http-response ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D script-path=https://raw.githubusercontent.com/yichahucha/surge/master/NetflixRatings.js,requires-body=1
+http-response ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D requires-body=1,script-path=https://raw.githubusercontent.com/yichahucha/surge/master/NetflixRatings.js
 
 [MITM]
 hostname = ios.prod.ftl.netflix.com
