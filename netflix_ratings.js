a619ddfb7fd4567f4278fa4d1a265cb014d8c3f4
0x1 <xlitao@icloud.com>
2019-10-29 16:25:59 +0800

    Update netflix_ratings.js

--- netflix_ratings.js
+++ netflix_ratings.js
@@ -1,10 +1,5 @@
 /*
-[Script]
-http-request ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D script-path=https://raw.githubusercontent.com/yichahucha/surge/master/netflix_ratings.js
-http-response ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D requires-body=1,script-path=https://raw.githubusercontent.com/yichahucha/surge/master/netflix_ratings.js
-
-[MITM]
-hostname = ios.prod.ftl.netflix.com
+https://github.com/yichahucha/surge/blob/master/README.md
  */
 
 const consoleLog = false;
