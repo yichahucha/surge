b300133406d2baa78a170ad9f533313375269e57
0x1 <xlitao@icloud.com>
2019-10-31 09:22:20 +0800

    Update cron_daily.js

--- cron_daily.js
+++ cron_daily.js
@@ -1,5 +1,5 @@
 /*
-README：https://github.com/yichahucha/surge/blob/master/README.md
+GitHub：https://github.com/yichahucha/surge/tree/master
 每日打卡提醒（corn "0 9,18 * * 1-5" 周一到周五，早九晚六）+ 每日壹句（有道词典）+ 跳转钉钉打卡页面（下拉通知点击链接）
 */
 $httpClient.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
