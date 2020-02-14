6a4b5b93558aafe3c087f9e818c6a0ade9aa8048
bird <12595948+yichahucha@users.noreply.github.com>
2020-02-14 10:36:43 +0800

    Update tb_price.js

--- tb_price.js
+++ tb_price.js
@@ -215,41 +215,75 @@ Date.prototype.format = function (fmt) {
 function tool() {
     const isSurge = typeof $httpClient != "undefined"
     const isQuanX = typeof $task != "undefined"
+    const isResponse = typeof $response != "undefined"
+    const node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
     const notify = (title, subtitle, message) => {
         if (isQuanX) $notify(title, subtitle, message)
         if (isSurge) $notification.post(title, subtitle, message)
+        if (node) console.log(JSON.stringify({ title, subtitle, message }));
     }
-    const setCache = (value, key) => {
+    const write = (value, key) => {
         if (isQuanX) return $prefs.setValueForKey(value, key)
         if (isSurge) return $persistentStore.write(value, key)
     }
-    const getCache = (key) => {
+    const read = (key) => {
         if (isQuanX) return $prefs.valueForKey(key)
         if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
     }
     const get = (options, callback) => {
         if (isQuanX) {
             if (typeof options == "string") options = { url: options }
             options["method"] = "GET"
             $task.fetch(options).then(response => {
-                response["status"] = response.statusCode
-                callback(null, response, response.body)
+                callback(null, adapterStatus(response), response.body)
             }, reason => callback(reason.error, null, null))
         }
-        if (isSurge) $httpClient.get(options, callback)
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (node) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
     }
     const post = (options, callback) => {
         if (isQuanX) {
             if (typeof options == "string") options = { url: options }
             options["method"] = "POST"
             $task.fetch(options).then(response => {
-                response["status"] = response.statusCode
-                callback(null, response, response.body)
+                callback(null, adapterStatus(response), response.body)
             }, reason => callback(reason.error, null, null))
         }
-        if (isSurge) $httpClient.post(options, callback)
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (node) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
     }
-    return { isQuanX, isSurge, notify, setCache, getCache, get, post }
+    return { isQuanX, isSurge, isResponse, notify, write, read, get, post }
 }
 
 function Base64() {
