e7c51e626a81778664a40b979453e61f96c3a11c
bird <12595948+yichahucha@users.noreply.github.com>
2020-02-14 16:56:06 +0800

    Create jd_price_lite.js

---
+++ jd_price_lite.js
@@ -0,0 +1,174 @@
+/*
+README：https://github.com/yichahucha/surge/tree/master
+ */
+
+const path1 = "serverConfig";
+const path2 = "wareBusiness";
+const log = false;
+const url = $request.url;
+const body = $response.body;
+const $tool = tool();
+
+if (url.indexOf(path1) != -1) {
+    let obj = JSON.parse(body);
+    delete obj.serverConfig.httpdns;
+    $done({ body: JSON.stringify(obj) });
+}
+
+if (url.indexOf(path2) != -1) {
+    let obj = JSON.parse(body);
+    const floors = obj.floors;
+    const commodity_info = floors[floors.length - 1];
+    const shareUrl = commodity_info.data.property.shareUrl;
+    $done({ body });
+    request_history_price(shareUrl, function (data) {
+        if (data) {
+            if (data.ok == 1 && data.single) {
+                const lower = history_price_msg(data.single)
+                $tool.notify("", "", `${lower}\n\n👉 查看详情：http://tool.manmanbuy.com/historyLowest.aspx?url=${encodeURI(shareUrl)}`)
+            }
+            if (data.ok == 0 && data.msg.length > 0) {
+                $tool.notify("", "", `⚠️ ${data.msg}`)
+            }
+        }
+    })
+}
+
+function history_price_msg(data) {
+    const lower = data.lowerPriceyh;
+    const lower_date = changeDateFormat(data.lowerDateyh);
+    const lower_msg = "〽️ 历史最低到手价:   ¥" + String(lower) + "   " + lower_date
+    const curret_msg = (data.currentPriceStatus ? "   当前价格" + data.currentPriceStatus : "") + "   (仅供参考)";
+    return lower_msg + curret_msg;
+}
+
+function request_history_price(share_url, callback) {
+    const options = {
+        url: "https://apapia-history.manmanbuy.com/ChromeWidgetServices/WidgetServices.ashx",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 - mmbWebBrowse - ios"
+        },
+        body: "methodName=getBiJiaInfo_wxsmall&p_url=" + encodeURIComponent(share_url)
+    }
+    $tool.post(options, function (error, response, data) {
+        if (!error) {
+            callback(JSON.parse(data));
+            if (log) console.log("Data:\n" + data);
+        } else {
+            callback(null, null);
+            if (log) console.log("Error:\n" + error);
+        }
+    })
+}
+
+function changeDateFormat(cellval) {
+    const date = new Date(parseInt(cellval.replace("/Date(", "").replace(")/", ""), 10));
+    const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+    const currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    return date.getFullYear() + "-" + month + "-" + currentDate;
+}
+
+function tool() {
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (node) console.log(JSON.stringify({ title, subtitle, message }));
+    }
+    const setCache = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const getCache = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response.status) {
+            response["statusCode"] = response.status
+        } else if (response.statusCode) {
+            response["status"] = response.statusCode
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (node) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (node) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    return { isQuanX, isSurge, notify, setCache, getCache, get, post }
+}
+
+Array.prototype.insert = function (index, item) {
+    this.splice(index, 0, item);
+};
+
+Date.prototype.format = function (fmt) {
+    var o = {
+        "y+": this.getFullYear(),
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S+": this.getMilliseconds()
+    };
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            if (k == "y+") {
+                fmt = fmt.replace(RegExp.$1, ("" + o[k]).substr(4 - RegExp.$1.length));
+            }
+            else if (k == "S+") {
+                var lens = RegExp.$1.length;
+                lens = lens == 1 ? 3 : lens;
+                fmt = fmt.replace(RegExp.$1, ("00" + o[k]).substr(("" + o[k]).length - 1, lens));
+            }
+            else {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
+    }
+    return fmt;
+}
