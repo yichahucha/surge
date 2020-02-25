43bd7ad5389687d112df815b4d7dbd396326151c
tern <12595948+yichahucha@users.noreply.github.com>
2020-02-25 20:05:18 +0800

    Update tb_price_lite.js

--- tb_price_lite.js
+++ tb_price_lite.js
@@ -3,7 +3,6 @@ README：https://github.com/yichahucha/surge/tree/master
  */
 
 const $tool = new Tool()
-const $base64 = new Base64()
 const consoleLog = false
 const url = $request.url
 const body = $response.body
@@ -11,20 +10,25 @@ const path1 = "/amdc/mobileDispatch"
 const path2 = "/gw/mtop.taobao.detail.getdetail"
 
 if (url.indexOf(path1) != -1) {
-    let obj = JSON.parse($base64.decode(body))
-    let dns = obj.dns
-    if (dns && dns.length > 0) {
-        let i = dns.length;
+    if ($tool.isResponse) {
+        $tool.notify("", "", "脚本已更新，此配置方式已废弃，请参考文档更新配置并更新脚本" + "\nGitHub:  " + "https://github.com/yichahucha/surge/tree/master")
+        $done()
+    } else {
+        let body = $request.body
+        let json = Qs2Json(body)
+        let domain = json.domain.split(" ")
+        let i = domain.length;
         while (i--) {
-            const element = dns[i];
-            let host = "trade-acs.m.taobao.com"
-            if (element.host == host) {
-                element.ips = []
-                if (consoleLog) console.log(JSON.stringify(element))
+            const block = "trade-acs.m.taobao.com"
+            const element = domain[i];
+            if (element == block) {
+                domain.splice(i, 1);
             }
         }
+        json.domain = domain.join(" ")
+        body = Json2Qs(json)
+        $done({ body })
     }
-    $done({ body: $base64.encode(JSON.stringify(obj)) })
 }
 
 if (url.indexOf(path2) != -1) {
@@ -151,6 +155,29 @@ function dateFormat(cellval) {
     const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
     const currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
     return date.getFullYear() + "-" + month + "-" + currentDate;
+}
+
+function Qs2Json(url) {
+    url = url == null ? window.location.href : url;
+    var search = url.substring(url.lastIndexOf("?") + 1);
+    var obj = {};
+    var reg = /([^?&=]+)=([^?&=]*)/g;
+    search.replace(reg, function (rs, $1, $2) {
+        var name = decodeURIComponent($1);
+        var val = decodeURIComponent($2);
+        val = String(val);
+        obj[name] = val;
+        return rs;
+    });
+    return obj;
+}
+
+function Json2Qs(json) {
+    var temp = [];
+    for (var k in json) {
+        temp.push(k + "=" + json[k]);
+    }
+    return temp.join("&");
 }
 
 Array.prototype.insert = function (index, item) {
