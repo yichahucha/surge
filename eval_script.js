ee4a7993d26b50dbcbb5df85582959563021cea5
goose <yichahucha@users.noreply.github.com>
2020-03-01 20:54:01 +0800

    optimize

--- eval_script.js
+++ eval_script.js
@@ -5,17 +5,17 @@
  * [task_local]
  * 0 0 * * * eval_script.js
  * 
- * 2.__conf 配置说明：
+ * 2.__conf 配置脚本远程链接说明：
  * 
  * 参考下面 __conf 示例
  * 
- * [远程订阅脚本]
+ * [远程配置]
  * 参考示例：https://raw.githubusercontent.com/yichahucha/surge/master/sub_script.conf
  * 
- * [本地添加脚本远程连接]
+ * [本地配置]
  * 格式为：远程脚本的链接 url 匹配脚本对应的正则1,匹配脚本对应的正则2
  * 
- * 如果是本地添加需要修改配置文件的本地脚本为此脚本，例如之前京东 jd_price.js 改为 eval_script.js 即可：
+ * 如果是本地配置需要修改配置文件的本地脚本为此脚本，例如之前京东 jd_price.js 改为 eval_script.js 即可：
  * [rewrite_local]
  * # ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) url script-response-body jd_price.js
  * ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) url script-response-body eval_script.js
@@ -30,12 +30,12 @@ const __conf = String.raw`
 
 
 [remote]
-//配置脚本远程订阅
+//远程配置
 https://raw.githubusercontent.com/yichahucha/surge/master/sub_script.conf
 
 
 [local]
-//配置脚本远程连接
+//本地配置
 //京东
 //https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js url ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig)
 
