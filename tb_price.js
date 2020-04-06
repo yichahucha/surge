dd286c9ce63bde67f39e3710eb330783e7c63c36
goose <goose@gmail.com>
2020-04-06 21:28:34 +0800

    fix

--- tb_price.js
+++ tb_price.js
@@ -30,9 +30,7 @@ if (url.indexOf(path1) != -1) {
     } else {
         let headers = $request.headers
         let body = $request.body
-        $tool.notify("", "", headers["User-Agent"])
         if (headers["User-Agent"].indexOf("%E6%89%8B%E6%9C%BA%E6%B7%98%E5%AE%9D") != -1) {
-            $tool.notify("", "", "修改")
             let json = Qs2Json(body)
             let domain = json.domain.split(" ")
             let i = domain.length;
--- tb_price_lite.js
+++ tb_price_lite.js
@@ -28,19 +28,22 @@ if (url.indexOf(path1) != -1) {
         body = $base64.encode(JSON.stringify(obj))
         $done({ body })
     } else {
+        let headers = $request.headers
         let body = $request.body
-        let json = Qs2Json(body)
-        let domain = json.domain.split(" ")
-        let i = domain.length;
-        while (i--) {
-            const block = "trade-acs.m.taobao.com"
-            const element = domain[i];
-            if (element == block) {
-                domain.splice(i, 1);
+        if (headers["User-Agent"].indexOf("%E6%89%8B%E6%9C%BA%E6%B7%98%E5%AE%9D") != -1) {
+            let json = Qs2Json(body)
+            let domain = json.domain.split(" ")
+            let i = domain.length;
+            while (i--) {
+                const block = "trade-acs.m.taobao.com"
+                const element = domain[i];
+                if (element == block) {
+                    domain.splice(i, 1);
+                }
             }
+            json.domain = domain.join(" ")
+            body = Json2Qs(json)
         }
-        json.domain = domain.join(" ")
-        body = Json2Qs(json)
         $done({ body })
     }
 }
