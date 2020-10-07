4c200b25bb54ec97bb614ab3fd99b9e7bcf33142
tern <12595948+yichahucha@users.noreply.github.com>
2020-10-07 13:41:50 +0800

    Update jd_price.js

--- jd_price.js
+++ jd_price.js
@@ -12,63 +12,63 @@ const body = $response.body;
 const headers = $request.headers;
 const $tool = tool();
 
-if (url.indexOf(path1) != -1) {
-    let obj = JSON.parse(body);
-    if (headers["User-Agent"].indexOf(userAgent) != -1) {
+if (headers["User-Agent"].indexOf(userAgent) != -1) {
+    if (url.indexOf(path1) != -1) {
+        let obj = JSON.parse(body);
         delete obj.serverConfig.httpdns;
         delete obj.serverConfig.dnsvip;
         delete obj.serverConfig.dnsvip_v6;
+        $done({ body: JSON.stringify(obj) });
     }
-    $done({ body: JSON.stringify(obj) });
-}
 
-if (url.indexOf(path3) != -1) {
-    let obj = JSON.parse(body);
-    if (headers["User-Agent"].indexOf(userAgent) != -1) {
+    if (url.indexOf(path3) != -1) {
+        let obj = JSON.parse(body);
         delete obj.data.JDHttpToolKit.httpdns;
         delete obj.data.JDHttpToolKit.dnsvipV6;
+        $done({ body: JSON.stringify(obj) });
     }
-    $done({ body: JSON.stringify(obj) });
-}
 
-if (url.indexOf(path2) != -1) {
-    let obj = JSON.parse(body);
-    const floors = obj.floors;
-    const commodity_info = floors[floors.length - 1];
-    const shareUrl = commodity_info.data.property.shareUrl;
-    request_history_price(shareUrl, function (data) {
-        if (data) {
-            const lowerword = adword_obj();
-            lowerword.data.ad.textColor = "#fe0000";
-            let bestIndex = 0;
-            for (let index = 0; index < floors.length; index++) {
-                const element = floors[index];
-                if (element.mId == lowerword.mId) {
-                    bestIndex = index + 1;
-                    break;
-                } else {
-                    if (element.sortId > lowerword.sortId) {
-                        bestIndex = index;
+    if (url.indexOf(path2) != -1) {
+        let obj = JSON.parse(body);
+        const floors = obj.floors;
+        const commodity_info = floors[floors.length - 1];
+        const shareUrl = commodity_info.data.property.shareUrl;
+        request_history_price(shareUrl, function (data) {
+            if (data) {
+                const lowerword = adword_obj();
+                lowerword.data.ad.textColor = "#fe0000";
+                let bestIndex = 0;
+                for (let index = 0; index < floors.length; index++) {
+                    const element = floors[index];
+                    if (element.mId == lowerword.mId) {
+                        bestIndex = index + 1;
                         break;
+                    } else {
+                        if (element.sortId > lowerword.sortId) {
+                            bestIndex = index;
+                            break;
+                        }
                     }
                 }
+                if (data.ok == 1 && data.single) {
+                    const lower = lowerMsgs(data.single);
+                    const detail = priceSummary(data);
+                    const tip = data.PriceRemark.Tip + "（仅供参考）";
+                    lowerword.data.ad.adword = `${lower} ${tip}\n${detail}`;
+                    floors.insert(bestIndex, lowerword);
+                }
+                if (data.ok == 0 && data.msg.length > 0) {
+                    lowerword.data.ad.adword = "⚠️ " + data.msg;
+                    floors.insert(bestIndex, lowerword);
+                }
+                $done({ body: JSON.stringify(obj) });
+            } else {
+                $done({ body });
             }
-            if (data.ok == 1 && data.single) {
-                const lower = lowerMsgs(data.single);
-                const detail = priceSummary(data);
-                const tip = data.PriceRemark.Tip + "（仅供参考）";
-                lowerword.data.ad.adword = `${lower} ${tip}\n${detail}`;
-                floors.insert(bestIndex, lowerword);
-            }
-            if (data.ok == 0 && data.msg.length > 0) {
-                lowerword.data.ad.adword = "⚠️ " + data.msg;
-                floors.insert(bestIndex, lowerword);
-            }
-            $done({ body: JSON.stringify(obj) });
-        } else {
-            $done({ body });
-        }
-    });
+        });
+    }
+} else {
+    $done({ body });
 }
 
 function lowerMsgs(data) {
