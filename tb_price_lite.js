783b5571f1fa9e84259fc60723c53bc3b9a560e3
yichahucha <12595948+yichahucha@users.noreply.github.com>
2021-03-31 22:35:46 +0800

    Update tb_price_lite.js

--- tb_price_lite.js
+++ tb_price_lite.js
@@ -86,7 +86,7 @@ function priceSummary(data) {
         } else if (item.Name == "618价格") {
             item.Name = "六一八价格"
         }
-        summary += `\n${item.Name}   ${String(parseInt(item.Price))}   ${item.Date}   ${item.Difference}`
+        summary += `\n${item.Name}   ${"￥"+String(parseInt(item.Price).substr(1))}   ${item.Date}   ${item.Difference}`
     })
     return summary
 }
