821c6ba9aae76bb6ce9c066ee0e5ba6125c05e1d
yichahucha <12595948+yichahucha@users.noreply.github.com>
2021-03-31 22:09:33 +0800

    Update tb_price_lite.js

--- tb_price_lite.js
+++ tb_price_lite.js
@@ -60,7 +60,7 @@ if (url.indexOf(path2) != -1) {
                 const lower = lowerMsgs(data.single)
                 const detail = priceSummary(data)
                 const tip = data.PriceRemark.Tip + "（仅供参考）"
-                $tool.notify("", "", `${lower} ${tip}${detail}\n👉查看详情：http://tool.manmanbuy.com/historyLowest.aspx?url=${encodeURI(shareUrl)}`)
+                $tool.notify("", "", `${lower}${detail}\n👉查看详情：http://tool.manmanbuy.com/historyLowest.aspx?url=${encodeURI(shareUrl)}`)
             }
             if (data.ok == 0 && data.msg.length > 0) {
                 $tool.notify("", "", `⚠️ ${data.msg}`)
@@ -72,7 +72,7 @@ if (url.indexOf(path2) != -1) {
 function lowerMsgs(data) {
     const lower = data.lowerPriceyh
     const lowerDate = dateFormat(data.lowerDateyh)
-    const lowerMsg = "🫖 历史最低到手价：¥" + String(lower) + ` (${lowerDate}) `
+    const lowerMsg = "历史最低到手价：¥" + String(lower) + ` (${lowerDate}) `
     return lowerMsg
 }
 
@@ -136,7 +136,7 @@ function historySummary(single) {
             }
         }
     });
-    return [lowest30, lowest90, lowest180, lowest360];
+    return [lowest30, lowest90, lowest180];
 }
 
 function difference(currentPrice, price) {
